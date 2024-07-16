2fee34861d2cf83e4b9d185198a43b6382d728cb
AlanRexcop <longbuihai394@gmail.com>
2024-07-16 13:14:14 +0700

    add recv_all

--- modules/shared.py
+++ modules/shared.py
@@ -38,3 +38,31 @@ ERROR_CODES = {
 MAX_BUF = 65536
 DEFAULT_SERVER_PORT = 8888
 DEFAULT_FORMAT = 'utf-8'
+
+def recv_data(sock, file_path, offset, count):
+    """receive data and write to file"""
+    total_written = 0
+    buffer = bytearray(MAX_BUF)
+    view = memoryview(buffer)
+    with open(file_path, "r+b") as f:
+        f.seek(offset)
+        while total_written < count:
+            read_size = min(count - total_written, MAX_BUF)
+            received = sock.recv_into(view[:read_size])
+            if not received:
+                raise OSError("Other side abruptly disconnected.")
+            total_written += received
+            f.write(view[:received])
+
+
+def recv_all(sock, n):
+    """recv n bytes or return except if EOF is hit."""
+    data = bytearray(n)
+    view = memoryview(data)
+    total_received = 0
+    while total_received < n:
+        received = sock.recv_into(view[total_received:], n - total_received)
+        if not received:
+            raise OSError("Other side abruptly disconnected.")
+        total_received += received
+    return data
