8c88aabc6a47f26369894426fc7361020b59bb60
HTuanPhong <tuanphongwithau@gmail.com>
2024-07-24 07:32:35 +0700

    messenger class

--- modules/message.py
+++ modules/message.py
@@ -5,151 +5,139 @@ file_path is file path relative to server's root not path on our machine.
 
 import struct
 import json
-from socket import AF_INET, socket, SOCK_STREAM
+import threading
+from socket import AF_INET, socket, SOCK_STREAM, SHUT_RDWR
 from modules.shared import *
+from queue import Queue
 
 FORMAT = DEFAULT_FORMAT
 
+lock = threading.Lock()
+messengers = []
 
-class error(Exception):
+
+def disconnect_all():
+    with lock:
+        global messengers
+        for msgr in messengers:
+            msgr.shutdown()
+        messengers = []
+
+
+class messengerError(Exception):
     pass
 
 
-def raise_err(sock):
-    err_len = struct.unpack(">B", recv_all(sock, 1))[0]
-    err_msg = recv_all(sock, err_len).decode(FORMAT)
+class messenger:
+    def __init__(self, host, port):
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.connect((host, port))
+        with lock:
+            messengers.append(self)
 
-    raise error(err_msg)
+    def _raise_err(self):
+        err_len = struct.unpack(">B", recv_all(self.sock, 1))[0]
+        err_msg = recv_all(self.sock, err_len).decode(FORMAT)
+        raise messengerError(err_msg)
 
-
-def send_RRQ(address, file_path):
-    """send Read request"""
-    sock = socket(AF_INET, SOCK_STREAM)
-    try:
-        sock.connect(address)
-        sock.sendall(struct.pack(">BB", RRQ, len(file_path)) + file_path.encode(FORMAT))
-        result_opcode = struct.unpack(">B", recv_all(sock, 1))[0]
+    def send_RRQ(self, file_path):
+        """send Read request"""
+        self.sock.sendall(
+            struct.pack(">BB", RRQ, len(file_path)) + file_path.encode(FORMAT)
+        )
+        result_opcode = struct.unpack(">B", recv_all(self.sock, 1))[0]
 
         if result_opcode == SUCCESS:
-            return struct.unpack(">Q", recv_all(sock, 8))[0]
+            return struct.unpack(">Q", recv_all(self.sock, 8))[0]
         else:
-            raise_err(sock)
-    finally:
-        sock.close()
+            self._raise_err()
 
-
-def send_WRQ(address, file_path, size):
-    """send Write request"""
-    sock = socket(AF_INET, SOCK_STREAM)
-    try:
-        sock.connect(address)
-        sock.sendall(
+    def send_WRQ(self, file_path, size):
+        """send Write request"""
+        self.sock.sendall(
             struct.pack(">BB", WRQ, len(file_path))
             + file_path.encode(FORMAT)
             + struct.pack(">Q", size)
         )
-        result_opcode = struct.unpack(">B", recv_all(sock, 1))[0]
+        result_opcode = struct.unpack(">B", recv_all(self.sock, 1))[0]
 
         if result_opcode == ERROR:
-            raise_err(sock)
-    finally:
-        sock.close()
+            self._raise_err()
 
-
-def send_DRRQ(address, file_path, offset, count):
-    """send Data read request"""
-    sock = socket(AF_INET, SOCK_STREAM)
-    try:
-        sock.connect(address)
-        sock.sendall(
+    def send_DRRQ(self, file_path, offset, length, local_file_path):
+        """send Data read request"""
+        self.sock.sendall(
             struct.pack(">BB", DRRQ, len(file_path))
             + file_path.encode(FORMAT)
-            + struct.pack(">QQ", offset, count)
+            + struct.pack(">QQ", offset, length)
         )
-        result_opcode = struct.unpack(">B", recv_all(sock, 1))[0]
+        result_opcode = struct.unpack(">B", recv_all(self.sock, 1))[0]
 
         if result_opcode == SUCCESS:
-            return recv_all(sock, count)
+            return recv_data(self.sock, local_file_path, offset, length)
         else:
-            raise_err(sock)
-    finally:
-        sock.close()
+            self._raise_err()
 
-
-def send_DWRQ(address, file_path, offset, length, local_file_path):
-    """send Data write request"""
-    sock = socket(AF_INET, SOCK_STREAM)
-    try:
-        sock.connect(address)
-        sock.sendall(
+    def send_DWRQ(self, file_path, offset, length, local_file_path):
+        """send Data write request"""
+        self.sock.sendall(
             struct.pack(">BB", DWRQ, len(file_path))
             + file_path.encode(FORMAT)
             + struct.pack(">QQ", offset, length)
         )
         with open(local_file_path, "rb") as f:
-            sock.sendfile(f, offset, length)
-        result_opcode = struct.unpack(">B", recv_all(sock, 1))[0]
+            self.sock.sendfile(f, offset, length)
+        result_opcode = struct.unpack(">B", recv_all(self.sock, 1))[0]
 
         if result_opcode == ERROR:
-            raise_err(sock)
-    finally:
-        sock.close()
+            self._raise_err()
 
-
-def send_FWRQ(address, file_path):
-    """send Finish write request"""
-    sock = socket(AF_INET, SOCK_STREAM)
-    try:
-        sock.connect(address)
-        sock.sendall(
+    def send_FWRQ(self, file_path):
+        """send Finish write request"""
+        self.sock.sendall(
             struct.pack(">BB", FWRQ, len(file_path)) + file_path.encode(FORMAT)
         )
-        result_opcode = struct.unpack(">B", recv_all(sock, 1))[0]
+        result_opcode = struct.unpack(">B", recv_all(self.sock, 1))[0]
         if result_opcode == ERROR:
-            raise_err(sock)
-    finally:
-        sock.close()
+            self._raise_err()
 
-
-def send_DRQ(address, file_path):
-    """send Delete request"""
-    sock = socket(AF_INET, SOCK_STREAM)
-    try:
-        sock.connect(address)
-        sock.sendall(struct.pack(">BB", DRQ, len(file_path)) + file_path.encode(FORMAT))
-        result_opcode = struct.unpack(">B", recv_all(sock, 1))[0]
+    def send_DRQ(self, file_path):
+        """send Delete request"""
+        self.sock.sendall(
+            struct.pack(">BB", DRQ, len(file_path)) + file_path.encode(FORMAT)
+        )
+        result_opcode = struct.unpack(">B", recv_all(self.sock, 1))[0]
         if result_opcode == ERROR:
-            raise_err(sock)
-    finally:
-        sock.close()
+            self._raise_err()
 
+    def sub_DTRQ(self):
+        """send Directory subcribe"""
+        self.sock.sendall(struct.pack(">B", DTRQ))
 
-def send_DTRQ(address):
-    """send Directory request"""
-    sock = socket(AF_INET, SOCK_STREAM)
-    try:
-        sock.connect(address)
-        sock.sendall(struct.pack(">B", DTRQ))
-        result_opcode = struct.unpack(">B", recv_all(sock, 1))[0]
-
+    def recv_DTRQ(self):
+        """receive Directory"""
+        result_opcode = struct.unpack(">B", recv_all(self.sock, 1))[0]
         if result_opcode == SUCCESS:
-            data_length = struct.unpack(">I", recv_all(sock, 4))[0]
-            directory_dict = json.loads(recv_all(sock, data_length).decode(FORMAT))
+            data_length = struct.unpack(">I", recv_all(self.sock, 4))[0]
+            directory_dict = json.loads(recv_all(self.sock, data_length).decode(FORMAT))
             return directory_dict
         else:
-            raise_err(sock)
-    finally:
-        sock.close()
+            self._raise_err()
 
-
-def send_FRQ(address, path):
-    """send Create folder request"""
-    sock = socket(AF_INET, SOCK_STREAM)
-    try:
-        sock.connect(address)
-        sock.sendall(struct.pack(">BB", FRQ, len(path)) + path.encode(FORMAT))
-        result_opcode = struct.unpack(">B", recv_all(sock, 1))[0]
+    def send_FRQ(self, path):
+        """send Create folder request"""
+        self.sock.sendall(struct.pack(">BB", FRQ, len(path)) + path.encode(FORMAT))
+        result_opcode = struct.unpack(">B", recv_all(self.sock, 1))[0]
         if result_opcode == ERROR:
-            raise_err(sock)
-    finally:
-        sock.close()
+            self._raise_err()
+
+    def shutdown(self):
+        if self.sock:
+            self.sock.shutdown(SHUT_RDWR)
+
+    def close(self):
+        if self.sock:
+            self.sock.close()
+        with lock:
+            if self in messengers:
+                messengers.remove(self)
