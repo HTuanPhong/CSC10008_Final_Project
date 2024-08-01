bab0fe9acc088db8c93d12d1efcf43d8ec167a6a
AlanRexcop <longbuihai394@gmail.com>
2024-08-02 03:08:15 +0700

    basically work, handle duplicate by make copy still got some user problem single file process read file for more detail

--- modules/process.py
+++ modules/process.py
@@ -1,3 +1,7 @@
+#TODO: find a way to handle duplicate file -> can easily fix with an app setting
+#Do something with error queue, only happen when file/folder get delete before process or connection error -> might just ignore
+#change progress callback to watch process instead of segment, incase processing multiple file at the same time => can be ignore
+
 import os
 from modules.shared import *
 from modules.message import *
@@ -8,38 +12,55 @@ import time
 
 simpleThreadNumber = 8
 MAX_CONNECTIONS = 8
-DEF_BUF = 2 ** 13 #8kB
-directory_path = None
-# HOST = None
-# PORT = None
-HOST = "localhost"  # IP adress server
-PORT = 8888  # Port is used by the server
+DEF_BUF = 2 ** 13   #8kB
+HOST = "localhost"  
+PORT = 8888  
+SETTING = "None"
 
 def set_address(host, port):
     global HOST, PORT
     HOST = host
     PORT = port
 
+def getSetting(overwrite: bool, remember: bool):
+    if(remember):
+        global SETTING 
+        if(overwrite):
+            SETTING = 'overwrite'
+        else:
+            SETTING = 'delete'
+
+
+DEBUG = True
+def debug(*msg):
+    if DEBUG:
+        print('debug:', msg)
+
+BUG = True      #set True if want bug to print out to console
+def bug(*msg):
+    if BUG:
+        print('bug:', msg)
+
 #region Manager
 #connections manager and process manager
-#use static amount of connections for now
 
 connections = []
 queue = []
 error_queue = []
+process_queue = []
 manager_error = 'null'
 manager_status = 'wait'
-# for i in range(MAX_CONNECTIONS):
-#     connections.append(messenger(HOST, PORT))
     
 def upload(client_paths, sizes, server_folder, callback = print):
     for client_path, size in zip(client_paths, sizes):
         process = Upload(client_path, server_folder, size, callback)
+        process._validPath()
         queue.append(process)
 
 def download(server_paths, sizes, client_folder, callback = print):
     for server_path, size in zip(server_paths, sizes): 
         process = Download(client_folder, server_path, size, callback)
+        process._validPath()
         queue.append(process)
 
 def manager(error_callback = print):
@@ -52,7 +73,9 @@ def manager(error_callback = print):
                 while (len(connections) < MAX_CONNECTIONS) and (len(connections) < connection_cnt):
                     connections.append(messenger(HOST, PORT))
                     
-                print('connection cnt:', connection_cnt, queue[-1].file_size, DEF_BUF)
+                debug('connection cnt:', connection_cnt, queue[-1].file_size, DEF_BUF)
+
+                #TODO: find better way to handle error process
                 queue[-1].startProcess(connections)
                 if(queue[-1].status == 'finish'):
                     queue.pop(-1)
@@ -66,7 +89,7 @@ def manager(error_callback = print):
                 connections[i].close()
                 connections.pop(i)
     
-    print('error queue', error_queue)
+    debug('error queue', error_queue)
     for i in range(len(connections)-1, -1, -1):
                 connections[i].close()
                 connections.pop(i)
@@ -74,7 +97,7 @@ def manager(error_callback = print):
 Manager = None
 
 def managerStart(error_callback):
-    global manager, manager_status
+    global Manager, manager_status
     manager_status = 'working'
     Manager = threading.Thread(target=manager, args=(error_callback,))
     Manager.start()
@@ -89,7 +112,6 @@ def managerPause(num: int):
 
 #endregion
 
-#yet to be done class
 #region Segment
 class Segment():
     def __init__(self, offset, length, process):
@@ -99,7 +121,6 @@ class Segment():
         self.thread = None
         self.mark_byte = 0
         self.progress = 0
-        self.cnt = 0
 
     def __str__(self):
         return f"({self.offset}, {self.length})"
@@ -109,11 +130,7 @@ class Segment():
             return
 
         try:
-            while(self.mark_byte < self.length and self.process.status != 'pause'):
-                # self.cnt += 1
-                # if self.cnt == 100:
-                #     print('progress', self.offset, self.progress)
-                #     self.cnt = 0
+            while(self.mark_byte < self.length and self.process.status != 'pause'):\
                 
                 if isinstance(self.process, Upload):
                     messenger.send_DWRQ(
@@ -136,10 +153,11 @@ class Segment():
                     self.mark_byte += min(MAX_BUF, self.length - self.mark_byte)
                     self.progress = self.mark_byte / self.length
                     self.process.callback(self.process.server_path, self.process.client_path, self.mark_byte)
+
         except Exception as error:
-            #dev error here
-            print("segment error:", error)
-            pass
+            bug("segment error:", error)
+            raise error
+
         finally:
             self.offset += self.mark_byte
             self.length -= self.mark_byte
@@ -149,7 +167,6 @@ class Segment():
         self.thread = threading.Thread(target= self.startProcess, args=(connection, ))
         self.thread.start()
 #endregion
-
 #region Process
 class Process():
     """
@@ -160,7 +177,8 @@ class Process():
         self.server_path = server_path
         self.file_size = size
         self.callback = callback
-
+        self.overwrite = False
+        
         self.segments = []
         self.connections = []
         self.status = 'wait'
@@ -174,15 +192,15 @@ class Process():
         pass
     def _finish():
         pass
-    def _checkPath(self):
+    def _validPath(self):   #checking path and raise exception
         pass
 
     def _fragment(self):
-        connection_ = len(self.connections)
-        print('segment', connection_)
-        segment_length = self.file_size // connection_
+        amount = len(self.connections)
+        debug('segment', amount)
+        segment_length = self.file_size // amount
         
-        for i in range(connection_):
+        for i in range(amount):
             offset = i * segment_length
             length = min(segment_length, self.file_size - offset)
             self.segments.append(Segment(offset, length, self))
@@ -194,18 +212,15 @@ class Process():
     #need to handle differently went update to dynamic connection
     def startProcess(self, connections):
         try:
-            if self.status == 'wait':
-                self._start(connections)
-                self._fragment()
+            self._validPath()
+            self.connections = connections
+            self._start()
+            self._fragment()
+            self.continueProcess()
 
-                self.continueProcess()
-
-            else:
-                raise Exception('already uploading')
-                #for handling with client pressing button multiple time 
         except Exception as error:
             self.callback('start error:', error)
-            self.status = 'wait'
+            self.status = 'error'
             self.error = error
             raise error
 
@@ -215,7 +230,7 @@ class Process():
             segment._start(connection)
 
         for segment in self.segments:
-                    segment.thread.join()
+            segment.thread.join()
 
         if(self.status != 'pause' or self.status != 'error'):
             self._finish(self.connections[0])
@@ -252,26 +267,30 @@ class Upload(Process):
     def __repr__(self):
         return f"({self.client_path_path}, {self.destinate_path}, {self.getProgress()})"
 
-    def _start(self, connections):
+    def _validPath(self):
+        if not os.path.exists(self.client_path):
+            raise FileNotFoundError('cant find file to upload')
+            
+        if os.path.isdir(self.client_path):
+            raise NotImplementedError('Function don\'t support')
+        
+        file_name = os.path.basename(os.path.normpath(self.client_path))
+        self.destinate_path = os.path.join(self.server_path, file_name)
+
+        # if SETTING == "None":
+        #     try:
+        #         send_RRQ(self.client_path, self.destinate_path)
+        #     except:
+        #         pass
+
+    def _start(self):
         try:
-            if not os.path.exists(self.client_path):
-                raise FileNotFoundError('cant find file to upload')
-            
-            if os.path.isdir(self.client_path):
-                raise NotImplementedError('chua cai :v')
-            
-            file_name = os.path.basename(os.path.normpath(self.client_path))
-            self.destinate_path = os.path.join(self.server_path, file_name)
-
             self.status = 'start'
-            self.connections = connections
-
-            #might raise file already exist or out of disk
-            connections[0].send_WRQ(self.destinate_path, self.file_size)
+            self.connections[0].send_WRQ(self.destinate_path, self.file_size)
                
         except Exception as error:
-            self.callback('Process error:', error)
             self.status = 'error'
+            bug(error)
             self.error = error
             raise error
 
@@ -290,28 +309,27 @@ class Download(Process):
 
     def __repr__(self):
         return f"({self.server_path}, {self.destinate_path}, {self.getProgress()})"
-    def check(self):
-        pass
-    def _start(self, connections):
+    def _validPath(self):
+        if(not os.path.exists(self.client_path)):    #path not found, dev error
+            raise FileNotFoundError('path not found')
+
+        if not os.path.isdir(self.client_path):
+            raise OSError('not folder path')
+        
+        file_name = os.path.basename(os.path.normpath(self.server_path))
+        self.destinate_path = get_unique_filename(os.path.join(self.client_path, file_name))
+
+
+        # if os.path.exists(self.destinate_path):   #file already exist on client, user decide
+        #     raise FileExistsError('file already exist')
+
+        if shutil.disk_usage(self.client_path)[2] < self.file_size:
+            raise OSError('client diskspace full')
+
+        self.destinate_path = self.destinate_path + '.downloading'
+
+    def _start(self):
         try:
-            if(not os.path.exists(self.client_path)):    #path not found, dev error
-                raise FileNotFoundError('path not found')
-            if not os.path.isdir(self.client_path):
-                raise OSError('not folder path')
-            
-            #this might not work on Linux
-            file_name = os.path.basename(os.path.normpath(self.server_path))
-            self.destinate_path = os.path.join(self.client_path, file_name)
-            if os.path.exists(self.destinate_path):   #file already exist on client, user decide
-                # print('file dest:', self.destinate_path)
-                # self.destinate_path = get_unique_filename(self.destinate_path)
-                raise FileExistsError('file already exist')
-
-            self.file_size = connections[0].send_RRQ(self.server_path)   #might raise file not exist
-            if shutil.disk_usage(self.client_path)[2] < self.file_size:
-                raise OSError('client diskspace full')
-
-            self.destinate_path = self.destinate_path + '.downloading'
             with open(self.destinate_path, "wb") as f:
                 f.seek(self.file_size - 1)
                 f.write(b"\0")
@@ -320,8 +338,8 @@ class Download(Process):
             self.connections = connections
 
         except Exception as error:
-            self.callback('start error:', error)
-            self.status = 'wait'
+            self.callback('Download process error:', error)
+            self.status = 'error'
             self.error = error
             raise error
 
