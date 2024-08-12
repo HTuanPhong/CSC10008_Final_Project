80a1f3e7932e6c736f3b36e9931d8a44d3e9648e
Nguyen Gia Huy <giahuydev26@gmail.com>
2024-08-12 18:54:02 +0700

    UI v5.0

--- FrontEnd.py
+++ FrontEnd.py
@@ -28,7 +28,7 @@ hostip = socket.gethostbyname(hostname)
 
 #============================================================={{ configure WINDOW }}===========================================================
 # Set mode and default theme
-customtkinter.set_appearance_mode("System")
+customtkinter.set_appearance_mode("Light")
 customtkinter.set_default_color_theme("blue")
 
 # Define constant
